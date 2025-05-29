bd523dd664853b6f062cba59f8e4a049c0578a5b
ci220 <98071063+syafiqnzr@users.noreply.github.com>
2025-05-30 06:06:44 +0800

    zzz

--- app.py
+++ app.py
@@ -107,12 +107,11 @@ def inject_user():
 
 # ---- Database Config ----
 db = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
+    host="localhost",
+    user="syafiq",
+    password="passwordku123",
+    database="nama_database"
 )
-
 
 cursor = db.cursor(dictionary=True)
 
